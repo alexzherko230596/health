54e07da5daed928905dbb17597e429856fe27d27
Alex <alexzherko@MacBook-Air-Alex.local>
2021-07-14 18:57:05 +0200

    New block is carousel

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrowL.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrowright.png"></button>'
+    });
+});     
